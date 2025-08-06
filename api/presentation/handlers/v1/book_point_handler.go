f791cdf8327847f634b1323b500834bfb126b22b
Lukeuke <lukasz.mrowiec10102004@gmail.com>
2025-08-06 21:49:19 +0200

    Changed error result handling

--- api/presentation/handlers/v1/book_point_handler.go
+++ api/presentation/handlers/v1/book_point_handler.go
@@ -10,6 +10,7 @@ import (
 	helpers "szyszko-api/application/helpers"
 
 	dto "szyszko-api/presentation/dto/book_point"
+	results "szyszko-api/presentation/dto/common"
 	"szyszko-api/presentation/handlers/v1/middlewares"
 
 	"github.com/gin-gonic/gin"
@@ -42,14 +43,14 @@ func (h *BookPointHandler) getAllBookPoints(c *gin.Context) {
 	query, err := helpers.TryBindDataQuery(c)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, results.ErrorResult[error](err))
 		return
 	}
 
 	result, err := h.uow.BookPointRepo.GetAll(c.Request.Context(), query)
 	if err != nil {
 		log.Printf("GetAll error: %v", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, results.ErrorResult[error](err))
 		return
 	}
 
@@ -63,14 +64,14 @@ func (h *BookPointHandler) getBookPointByID(c *gin.Context) {
 
 	if err != nil {
 		log.Printf("Błąd parsowania UUID: %v\n", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, results.ErrorResult[error](err))
 		return
 	}
 
 	point, err := h.uow.BookPointRepo.GetByID(c.Request.Context(), id)
 	if err != nil {
 		log.Printf("GetByID error: %v", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, results.ErrorResult[error](err))
 		return
 	}
 
@@ -82,7 +83,7 @@ func (h *BookPointHandler) insertNewBookPoint(c *gin.Context) {
 	var cmd dto.CreateBookPointCommand
 
 	if err := c.ShouldBindJSON(&cmd); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, results.ErrorResult[error](err))
 		return
 	}
 
@@ -95,7 +96,7 @@ func (h *BookPointHandler) insertNewBookPoint(c *gin.Context) {
 	id, err := h.uow.BookPointRepo.Insert(c.Request.Context(), &dao)
 	if err != nil {
 		log.Printf("insertNewBookPoint error: %v", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, results.ErrorResult[error](err))
 		return
 	}
 
@@ -109,14 +110,14 @@ func (h *BookPointHandler) approveBookPoint(c *gin.Context) {
 
 	if err != nil {
 		log.Printf("Błąd parsowania UUID: %v\n", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, results.ErrorResult[error](err))
 		return
 	}
 
 	err = h.uow.BookPointRepo.Approve(c.Request.Context(), id)
 	if err != nil {
 		log.Printf("approveBookPoint error: %v", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, results.ErrorResult[error](err))
 		return
 	}
 
@@ -129,14 +130,14 @@ func (h *BookPointHandler) deleteBookPoint(c *gin.Context) {
 
 	if err != nil {
 		log.Printf("Błąd parsowania UUID: %v\n", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, results.ErrorResult[error](err))
 		return
 	}
 
 	err = h.uow.BookPointRepo.Remove(c.Request.Context(), id)
 	if err != nil {
 		log.Printf("deleteBookPoint error: %v", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, results.ErrorResult[error](err))
 		return
 	}
 
