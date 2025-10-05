690617ebf3198adf3997c0c721d8b6b787183aaf
Lukeuke <lukasz.mrowiec10102004@gmail.com>
2025-10-05 14:19:41 +0200

    changes serverless.go - moved init() to Handler()

--- api/serverless/serverless.go
+++ api/serverless/serverless.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"szyszko-api/application"
 	"szyszko-api/application/helpers"
 	repository "szyszko-api/infrastructure/repositories"
@@ -12,31 +13,35 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var router http.Handler
-
-func init() {
-	log.Println("Initializing...")
-
-	if os.Getenv("VERCEL_ENV") != "production" {
-		_ = godotenv.Load()
-	}
-
-	log.Println("Loading config...")
-	helpers.InitJWTConfig()
-
-	url := helpers.MustGetenv("SUPABASE_URL")
-	key := helpers.MustGetenv("SUPABASE_KEY")
-
-	log.Println("Initializing supabase client...")
-	application.InitSupabaseClient(url, key)
-
-	uow := repository.NewUnitOfWork(application.SupabaseClient)
-
-	router = handlers.NewRouter(uow)
-
-	log.Println("Initialied.")
-}
+var (
+	router http.Handler
+	once   sync.Once
+)
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	once.Do(func() {
+		log.Println("Initializing...")
+
+		if os.Getenv("VERCEL_ENV") != "production" {
+			_ = godotenv.Load()
+		}
+
+		log.Println("Loading config...")
+		helpers.InitJWTConfig()
+
+		url := helpers.MustGetenv("SUPABASE_URL")
+		key := helpers.MustGetenv("SUPABASE_KEY")
+
+		log.Println("Initializing supabase client...")
+		application.InitSupabaseClient(url, key)
+
+		uow := repository.NewUnitOfWork(application.SupabaseClient)
+
+		router = handlers.NewRouter(uow)
+
+		log.Println("Initialized.")
+	})
+
 	router.ServeHTTP(w, r)
+	log.Println("Serving HTTP")
 }
