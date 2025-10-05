990e6f5a2469373f1aa77753666a8e14b5aae9f5
Łukasz Mrowiec <91629428+Lukeuke@users.noreply.github.com>
2025-10-05 14:02:36 +0200

    attempt to fix vercel (#35)
    * attempt to fix vercel 1
    
    * added logging for init

--- api/serverless/serverless.go
+++ api/serverless/serverless.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"log"
 	"net/http"
+	"os"
 	"szyszko-api/application"
 	"szyszko-api/application/helpers"
 	repository "szyszko-api/infrastructure/repositories"
@@ -13,17 +15,26 @@ import (
 var router http.Handler
 
 func init() {
-	_ = godotenv.Load()
+	log.Println("Initializing...")
+
+	if os.Getenv("VERCEL_ENV") != "production" {
+		_ = godotenv.Load()
+	}
+
+	log.Println("Loading config...")
 	helpers.InitJWTConfig()
 
 	url := helpers.MustGetenv("SUPABASE_URL")
 	key := helpers.MustGetenv("SUPABASE_KEY")
 
+	log.Println("Initializing supabase client...")
 	application.InitSupabaseClient(url, key)
 
 	uow := repository.NewUnitOfWork(application.SupabaseClient)
 
 	router = handlers.NewRouter(uow)
+
+	log.Println("Initialied.")
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
